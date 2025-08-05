bd0c230d4d884d2975d55be2682f7414b3a6133c
Priyank_Raspberry_Pi <ee3@pumpacademy.in>
2025-08-05 13:10:51 +0530

    Converted values from MFM into Readable format

--- main.c
+++ main.c
@@ -10,16 +10,22 @@ int main()
 {
 
 
-	if(modbus_tcp(MFM, 502, 1, 0x8006, 4))
+	if(modbus_tcp(MFM, 502, 1, 32774, 2))
 	{
 		printf("Done\n");
 
-		hex_float.value[0] = modbus_tcp_buffer[1];
-		hex_float.value[1] = modbus_tcp_buffer[0];
-		hex_float.value[2] = modbus_tcp_buffer[3];
-		hex_float.value[3] = modbus_tcp_buffer[2];
+		hex_float.value[0] = modbus_tcp_buffer[1] & 0xFF;
+		hex_float.value[1] = (modbus_tcp_buffer[1] >> 8) & 0xFF;
+		hex_float.value[2] = modbus_tcp_buffer[0] & 0xFF;
+		hex_float.value[3] = (modbus_tcp_buffer[0] >> 8) & 0xFF ;
 
 		printf("Voltage: %f\n",hex_float.result);
+
+		for(int i = 3; i >= 0 ; i--)
+			printf("%X ",hex_float.value[i]);
+
+		printf("\n");
+			
 	}
 	else
 		printf("Not Done\n");
