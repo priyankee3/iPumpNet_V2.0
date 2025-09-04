630deb5af8a40281c33ec559bc143658945bea03
Priyank Prajapati <ee3@pumpacademy.in>
2025-09-04 13:32:53 +0530

    Updated MQTT TCP socket issue in Publisher

--- mqtt_publish.c
+++ mqtt_publish.c
@@ -4,55 +4,6 @@ bool mqtt_publish(s8 *ip)
 	s32 rc;
 	s8* message = NULL;	
 	
-	/******************** MQTT Inialization ********************/
-	// Required before calling other mosquitto function
-	mosquitto_lib_init();
-	
-	/* Create a new client instance
-	   id = NULL -> ask the broker to generate a client id for us
-	   clean session = true -> the broker should remove old sessions when we connect
-	   obj = NULL -> we aren't passing any of our private data for call back */
-
-	mosq = mosquitto_new(NULL, true, NULL);
-	if( mosq == NULL )
-	{
-		fprintf(stderr,"Error in MQTT: Out of memory.\n");
-	}
-	
-	// Configure callback. This should be done before connecting ideally
-	mosquitto_connect_callback_set(mosq,on_connect);
-	mosquitto_publish_callback_set(mosq,on_publish);
-	
-	mosquitto_username_pw_set( mosq, "ee3", "Pump@2021" );
-	/* Connect to Broker on port 1883, with a keepalive of 60 seconds.
-	   This call make the socket connection only, it does not complete
-	   the MQTT CONNECT/CONNACK flow, you should use mosquitto_loop_start()
-	   or mosquitto_loop_forever() for processing net traffic */
-	rc = mosquitto_connect(mosq, ip, 1883, 10);
-	if(rc != MOSQ_ERR_SUCCESS)
-	{
-		//mosquitto_destroy(mosq);
-		fprintf(stderr,"Error in MQTT: %s\n", mosquitto_strerror(rc));
-		return 0;
-	}
-
-	// Runs network loop in a background thread, this calls returns quicky
-	/*rc = mosquitto_loop_start(mosq);
-	if(rc != MOSQ_ERR_SUCCESS)
-	{
-		printf("ok in publish\n");
-		//mosquitto_destroy(mosq);
-		fprintf(stderr,"Error in MQTT: %s\n", mosquitto_strerror(rc));
-		return 0;
-	}*/
-	
-	/* Publish Message 
-	   mosq - our client instance
-	   mid = NULL - we don't want to know what the message id for this message is 
-	   topic = "test/topic" - the topic on which this message will be publish
-	   payload - the actual payload
-	   qos = 2 - publish with QoS for this example 
-	   retain = false - do not use the retained message feature for this message */
 	
 	message = cJSON_Print(json_send);
 
@@ -66,9 +17,6 @@ bool mqtt_publish(s8 *ip)
 	}	
 
 	free(message);
-	mosquitto_disconnect(mosq);
-	mosquitto_destroy(mosq);
-	mosquitto_lib_cleanup();
 	return 1;
 }
 
